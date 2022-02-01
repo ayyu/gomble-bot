d8116d866c52e9f5bd9c26eeb07a282d22773d1d
ayyu <ayyu@uwaterloo.ca>
2022-02-01 10:33:49 -0500

    connect with db URL

--- db/connect.js
+++ db/connect.js
@@ -3,20 +3,4 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const assignExists = (obj, prop, val) => {
-	if (val != null) obj[prop] = val;
-}
-
-const dbOpts = {};
-assignExists(dbOpts, 'host', process.env.DB_HOST);
-assignExists(dbOpts, 'port', process.env.DB_PORT);
-assignExists(dbOpts, 'dialect', process.env.DB_DIALECT);
-assignExists(dbOpts, 'storage', process.env.DB_STORAGE);
-dbOpts['logging'] = false;
-
-module.exports = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASS,
-	dbOpts
-);
+module.exports = new Sequelize(proces.env.DATABASE_URL);
