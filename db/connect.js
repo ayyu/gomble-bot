c99ffd1d47952d470a0de9280039e55448a9b229
ayyu <ayyu@uwaterloo.ca>
2022-02-01 10:48:32 -0500

    require SSL and test auth

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,19 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		}
+	},
 	logging: false,
-	ssl: true,
 });
+
+sequelize
+	.authenticate()
+	.then(() => console.log(`Connection to database successful`))
+	.catch(err => console.error(`Unable to connect to the database:`, err));
+
+module.exports = sequelize
