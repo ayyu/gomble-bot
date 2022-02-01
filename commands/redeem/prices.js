98e6955363a77ea76c3d472622508de966cd7311
ayyu <ayyu@uwaterloo.ca>
2022-02-01 18:24:59 -0500

    pull prices

---
+++ commands/redeem/prices.js
@@ -0,0 +1,15 @@
+const { SlashCommandSubcommandBuilder } = require('@discordjs/builders');
+const { pricesKV } = require('../../db/keyv');
+
+const data = new SlashCommandSubcommandBuilder()
+  .setName('prices')
+  .setDescription(`See a list of prices set for redemptions`);
+
+module.exports = {
+  data,
+  async execute(interaction) {
+		const store = pricesKV.opts.store;
+		const rows = await store.query(`SELECT * FROM ${store.opts.table} WHERE key LIKE '${$store.namespace}:%'`);
+		console.log(rows);
+  },
+};
