6a8df9c0ed10f025e95a7c0f25079e0aa259919c
ayyu <ayyu@uwaterloo.ca>
2022-02-01 16:07:09 -0500

    maybe this will work

--- db/keyv.js
+++ db/keyv.js
@@ -1,12 +1,19 @@
 const Keyv = require('keyv');
 const dotenv = require('dotenv');
+const KeyvPostgres = require('@keyv/postgres');
 
 dotenv.config();
 
-dbURL = process.env.DATABASE_URL
+const store = new KeyvPostgres({
+	uri: process.env.DATABASE_URL,
+	ssl: {
+		require: true,
+		rejectUnauthorized: false,
+	}
+});
 
 module.exports = {
-	permsKV: new Keyv(dbURL, {namespace: 'perms'}),
-	wagesKV: new Keyv(dbURL, {namespace: 'wages'}),
-	pricesKV: new Keyv(dbURL, {namespace: 'prices'}),
+	permsKV: new Keyv(Object.assign({namespace: 'perms'}, store)),
+	wagesKV: new Keyv(Object.assign({namespace: 'wages'}, store)),
+	pricesKV: new Keyv(Object.assign({namespace: 'prices'}, store)),
 }
