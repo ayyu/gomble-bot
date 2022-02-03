73ca64d789dd24a3ec1952e0cedb04792059ccc6
ayyu <ayyu@uwaterloo.ca>
2022-02-03 18:28:01 -0500

    start prediction chain

--- commands/prediction/start.js
+++ commands/prediction/start.js
@@ -19,26 +19,12 @@ async function execute(interaction) {
 	if (!requireUnthreaded(interaction)) throw new Error(channelOnlyMsg);
 	const prompt = interaction.options.getString('prompt');
 
-	const reply = await interaction.reply({
-		content: '**Prediction**',
-		fetchReply: true,
-	});
-
-	/** @type {import('discord.js').ThreadChannel} */
-	const thread = await reply.startThread({
-		name: `${prompt}`,
-		autoArchiveDuration: 'MAX',
-	});
-
-	const prediction = await Prediction.create({
-		id: thread.id,
-		prompt,
-		open: true,
-	});
-
-	const embed = await startMessageEmbed(prediction);
-	await reply.edit({ embeds: [embed] });
-	await reply.pin();
+	await interaction.reply({ content: '**Prediction**', fetchReply: true })
+		.then(reply => reply.pin())
+		.then(reply => reply.startThread({ name: `${prompt}`, autoArchiveDuration: 'MAX' })
+			.then(thread => Prediction.create({ id: thread.id, prompt, open: true })
+				.then(prediction => startMessageEmbed(prediction)
+					.then(embed => reply.edit({ embeds: [embed] })))));
 }
 
 module.exports = {
