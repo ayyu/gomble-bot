ec4a55140be16a415c7a2aa25f16b3e5ee29aa7a
ayyu <ayyu@uwaterloo.ca>
2022-02-03 14:34:05 -0500

    move to inheritance model

--- commands/prediction.js
+++ commands/prediction.js
@@ -1,28 +1,8 @@
-const path = require('path');
-
-const { Collection } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
-const { absForEach } = require('../utils/fs');
+const { ParentCommand } = require('../models/Command');
 
 const data = new SlashCommandBuilder()
 	.setName('prediction')
 	.setDescription('Prediction management commands');
 
-const subcommands = new Collection();
-absForEach(path.resolve(__dirname, data.name), /\.js$/, (directory, file) => {
-	const command = require(`${directory}/${file}`);
-	data.addSubcommand(command.data);
-	subcommands.set(command.data.name, command);
-});
-
-module.exports = {
-	data,
-	async execute(interaction) {
-		if (interaction.options.getSubcommand(false)) {
-			const subcommand = subcommands.get(interaction.options.getSubcommand());
-			if (!subcommand) return;
-			await subcommand.execute(interaction);
-		}
-	},
-};
+module.exports = new ParentCommand(data, __dirname);
