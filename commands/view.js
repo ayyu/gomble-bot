20f4e4b0d6f3de933df386ba12bfcb09474e1151
ayyu <ayyu@uwaterloo.ca>
2022-02-04 20:59:56 -0500

    change to view subcommands

--- commands/view.js
+++ commands/view.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { ParentCommand } = require('../models/Command');
 
 const data = new SlashCommandBuilder()
-	.setName('user')
+	.setName('view')
 	.setDescription('View stats and settings');
 
 module.exports = new ParentCommand(data, __dirname);
