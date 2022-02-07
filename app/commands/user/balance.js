953aaf52cf491a5adadfde28ec2681373996fe6f
ayyu <ayyu@uwaterloo.ca>
2022-02-06 21:58:55 -0500

    log user

--- app/commands/user/balance.js
+++ app/commands/user/balance.js
@@ -23,12 +23,13 @@ async function execute(interaction) {
 			'balance',
 			[Sequelize.fn('SUM', Sequelize.col('bets.amount')), 'activeBetTotal'],
 		],
-		group: ['id'],
 		include: [{ model: Bet, attributes: []}],
+		group: ['id'],
 	})
 		.then(user => { if (!user) throw new Error(toggleMessages.registered[+false]); return user; })
 		.then(user => interaction.reply(
-			`**${target.user.tag}'s balance:** ${user.balance} + ${user.activeBetTotal} on active bets.`));
+			`**${target.user.tag}'s balance:** ${user.balance} + ${user.activeBetTotal} on active bets.`))
+		.then(console.log);
 }
 
 module.exports = new Command(data, execute);
